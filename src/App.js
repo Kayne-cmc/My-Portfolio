b35ef2e6dd990e3675063aaeae2ed4794e8adb04
Kayne-cmc <68172370+Kayne-cmc@users.noreply.github.com>
2020-07-31 09:47:12 -0400

    Text content changes

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
         // </nav> */}
     
         <p className="text">
-          Hi I'm Kayne and I am a 2A Mechanical Engineering student at the University of Waterloo. Ever since I was a kid, I would always visualize designs in my head. Whether its a solution or a design, every problem or difficult situation that I encounter, I would come up with a solution in my head. Without the proper experience, I was unable to make my designs come to life. As a future engineer I still hold the same goal I had as a child-to create awesome things that help others.
+          Hi, I'm Kayne and I am a 2A Mechanical Engineering student at the University of Waterloo. I love a nice relaxing day of coding or skateboarding but most of all I love learning new things! Ever since I was a kid, I would always question. Whether its a solution or a design, every problem or difficult situation that I encounter, I would come up with a solution in my head. Without the proper experience, I was unable to make my designs come to life. As a future engineer I still hold the same goal I had as a child-to create awesome things that help others.
         </p>        
 
 
@@ -80,7 +80,7 @@ function App() {
         <figure>
           <div className="container">
             <img src={Skateboarding} alt="Skateboarding"/>
-            <p className="skateboarding-text">I love skateboarding. To me, skateboarding is a discipline. The challenge of learning a new trick is more difficult than what others often assume. I keep track of my progress on my instagram page.</p>
+            <p className="skateboarding-text">Skateboarding was one of my very first hobbies as a kid. It has been very influencial in my life and has changed my perspective on learning. To me, skateboarding is a discipline. The challenge of learning a new trick is more difficult than what others often assume. I keep track of my progress on my instagram page.</p>
             <a className="instagram" href="https://www.instagram.com/kayne.skates/" target="blank"><i className="fab fa-instagram"></i></a>
           </div>
 
