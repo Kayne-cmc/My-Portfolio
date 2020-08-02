2620a0590f56186930631d9a57b968ff56771398
Kayne-cmc <68172370+Kayne-cmc@users.noreply.github.com>
2020-08-02 16:20:23 -0400

    Change Slideshow class to function & appearance changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Footer.css'
 
 function Footer() {
     return (
         <div className="Footer">
-            <h1>Get in Touch</h1>
+            <h1 className="header">Get in Touch</h1>
             <p className="text">Whether it's for new career opportunities or just to introduce yourself, I love meeting new people and I'm always looking to make new connections. Get in touch with me and we can set something up!</p>
         </div>
     );
--- src/Components/Slideshow/Slideshow.js
+++ src/Components/Slideshow/Slideshow.js
@@ -1,57 +1,81 @@
-import './Slideshow.css';
-import React, { Component } from "react";
+import "./Slideshow.css";
+import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Skateboarding from './images/Skateboarding.jpg';
-import Projects from './images/Project-picture.png';
-import Welding from './images/Welding.jpg';
+import Skateboarding from "./images/Skateboarding.jpg";
+import Projects from "./images/Project-picture.png";
+import Welding from "./images/Welding.jpg";
 
-const images = [
-  {src: Skateboarding},
-  {src: Projects},
-  {src: Welding}
-];
+function Slideshow() {
+  let settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnHover: true,
+    className: "slideshow",
+  };
 
-export default class Slideshow extends Component {
-  render() {
-    let settings = {
-      dots: true,
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2500,
-      pauseOnHover: true,
-      className: 'slideshow'
-    };
-
-    return (
-      <div>
-        <Slider {...settings}>
-          <div className="container">
-            <img src={Skateboarding} alt="Skateboarding" width="100%" />
-            <div className="image-content">
-              <p>Skateboarding was one of my very first hobbies as a kid. It has been very influencial in my life and has changed my perspective on learning. To me, skateboarding is a discipline. The challenge of learning a new trick is more difficult than what others often assume. I keep track of my progress on my instagram page.</p>
-              <a className="instagram" href="https://www.instagram.com/kayne.skates/" target="blank"><i className="fab fa-instagram"></i></a>
-            </div>
+  return (
+    <div>
+      <Slider {...settings}>
+        <div className="container">
+          <img src={Skateboarding} alt="Skateboarding" width="100%" />
+          <div className="image-content">
+            <p>
+              Skateboarding was one of my very first hobbies as a kid. It has
+              been very influencial in my life and has changed my perspective on
+              learning. To me, skateboarding is a discipline. The challenge of
+              learning a new trick is more difficult than what others often
+              assume. I keep track of my progress on my instagram page.
+            </p>
+            <a
+              className="instagram"
+              href="https://www.instagram.com/kayne.skates/"
+              target="blank"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
           </div>
-          <div className="container">
-            <img src={Projects}></img>
-            <div className="image-content">
-              <p>Working with 3D printing, my group and I printed parts to create a wind up climbing toy</p>
-              <a className="youtube" href="https://youtu.be/b8q1SQjiZmA" target="blank"><i className="fab fa-youtube"></i></a>
-            </div>
+        </div>
+        <div className="container">
+          <img src={Projects}></img>
+          <div className="image-content">
+            <p>
+              Working with 3D printing, my group and I printed parts to create a
+              wind up climbing toy
+            </p>
+            <a
+              className="youtube"
+              href="https://youtu.be/b8q1SQjiZmA"
+              target="blank"
+            >
+              <i className="fab fa-youtube"></i>
+            </a>
           </div>
-          <div className="container">
-            <img src={Welding}></img>
-            <div className="image-content">
-              <p>With some robotC coding, my group and I have programmed a robot to pick up objects.</p>
-              <a className="youtube" href="https://youtu.be/Od4rgrqviLM" target="blank"><i className="fab fa-youtube"></i></a>
-            </div>
+        </div>
+        <div className="container">
+          <img src={Welding}></img>
+          <div className="image-content">
+            <p>
+              With some robotC coding, my group and I have programmed a robot to
+              pick up objects.
+            </p>
+            <a
+              className="youtube"
+              href="https://youtu.be/Od4rgrqviLM"
+              target="blank"
+            >
+              <i className="fab fa-youtube"></i>
+            </a>
           </div>
-        </Slider>
-      </div>
-    );
-  }
+        </div>
+      </Slider>
+    </div>
+  );
 }
+
+export default Slideshow;
