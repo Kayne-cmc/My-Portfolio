dde28e2297eb9ec108239decd077fe0204c2ab66
Kayne-cmc <68172370+Kayne-cmc@users.noreply.github.com>
2020-08-05 01:34:38 -0400

    Format changes
    Included a photo and moved introduction

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import Testphoto from './Testphoto.jpg';
 
 function App() {
   return (
     <div className="App">
-      <p id="introduction" className="text">
-        Hi, I'm Kayne and I am a 2A Mechanical Engineering student at the University of Waterloo. I love a nice relaxing day of coding or skateboarding but most of all I love learning new things!<br></br> Ever since I was a kid, I was always interested in how things worked. Whether its a solution or a design, every problem or difficult situation that I encounter, I would come up with a solution in my head. Without the proper experience, I was unable to make my designs come to life. As a future engineer I still hold the same goal I had as a child-to create awesome things that help others.
+      <div className="opening">
+        <img className="portrait" src={Testphoto} alt="Portrait" />
+        <div className="opening-text">
+          <h1 className="header">Hi, I'm Kayne</h1>
+          <p style={{paddingBottom: 0}} className="text introduction">I am a 2A Mechanical Engineering student at the University of Waterloo. I love a nice relaxing day of coding or skateboarding but most of all I love learning new things and I love teaching others! From participating in the tutoring program at my highschool to becoming a self-employed math tutor, I have a developed an appreciation to the skill of teaching.</p>
+        </div>
+      </div>
+      
+      <p className="description text">
+         Ever since I was a kid, I was always interested in how things worked. Whether its a solution or a design, every problem or difficult situation that I encounter, I would come up with a solution in my head. Without the proper experience, I was unable to make my designs come to life. As a future engineer I still hold the same goal I had as a child-to create awesome things that help others.
       </p>
     </div>
   );
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,6 +6,7 @@ function Footer() {
         <div className="Footer">
             <h1 className="header">Get in Touch</h1>
             <p className="text">Whether it's for new career opportunities or just to introduce yourself, I love meeting new people and I'm always looking to make new connections. Get in touch with me and we can set something up!</p>
+            <a href="https://www.linkedin.com/in/kayne-chu-273988189?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BT3%2BXw5KSQ26AMHS5FYGRUA%3D%3D"><i className="icon-twitter"></i></a>
         </div>
     );
 }
